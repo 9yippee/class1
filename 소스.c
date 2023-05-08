54ed63c2861f4abcb2532c9b86e00fb8b59bf361
9yippee <gu9210@naver.com>
2023-05-08 11:46:29 +0900

    baekjoon

---
+++ 소스.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning (disable : 4996)
+
+int main()
+{
+	char arr[1000000];
+	int i=0, j=0, len;
+
+	scanf("%[^\n]", arr);
+	len = strlen(arr);
+
+	if (len == 1)
+	{
+		if (arr[i] == ' ')
+		{
+			printf("0");
+			return (0);
+		}
+	}
+	for (i = 1; i < len - 1; i++)
+	{
+		if (arr[i] == ' ')
+			j++;
+	}
+	printf("%d", j + 1);
+	
+	return (0);
+}
